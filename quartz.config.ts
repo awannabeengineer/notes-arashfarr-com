7a647709c73b8ef7132fadfd305b116f5041bcf9
Arash Farr <arash.moohan+huggingface@gmail.com>
2024-01-09 14:02:06 -0600

    configure: set site title and url

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "wbe",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.arashfarr.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
