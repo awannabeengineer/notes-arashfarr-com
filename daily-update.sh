14ae8d60d518a836e7f3f94fae9921f9c77b7c62
Arash Farr <arash.moohan+huggingface@gmail.com>
2024-05-31 14:19:31 -0500

    chnage directory to repo

--- daily-update.sh
+++ daily-update.sh
@@ -2,6 +2,7 @@
 
 # add to crontab with: 0 0 * * * /path/to/daily-update.sh for daily updates
 # update the content repository and push
+cd /home/wbe/Vaults/wbe || { echo "Failed to change directory"; exit 1; }
 cd ./content/ || exit
 git pull --no-edit
 cd ../ || exit
